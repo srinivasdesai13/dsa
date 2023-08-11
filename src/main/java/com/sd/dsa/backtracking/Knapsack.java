08d41bbcc7e59baa1b4d7749e87a51aa70f24e1a
srinivasdesai13 <srinivasdesai.13@gmail.com>
2023-08-11 14:37:39 -0700

    Update Knapsack.java
    another version

--- src/main/java/com/sd/dsa/backtracking/Knapsack.java
+++ src/main/java/com/sd/dsa/backtracking/Knapsack.java
@@ -8,6 +8,7 @@ public class Knapsack{
 	      int[] p = {4, 7, 5, 8};
 	      int capacity = 7; 
 	      int response = dfs(w,p,capacity,0,0,0);
+	      //int response = dfs2(w,p,capacity,0,0);
 	      System.out.println(response);
 	    }
 	    
@@ -32,6 +33,31 @@ public class Knapsack{
 	        
 	        
 	        
+	    }
+	    
+	    public static int dfs2(int[] w, int[] p, int capacity, int currentsum,  int index){
+	        
+	        if(index==w.length){
+	            return currentsum;
+	        }
+	        if(capacity<0){
+	            return 0;
+	        }
+	        
+	       
+	        if(capacity==0){
+	            return currentsum;
+	        }
+	        
+	        if(w[index]>capacity){
+	            return currentsum;
+	        }
+	       
+	        int inc = dfs2(w,p,capacity-w[index], currentsum+p[index], index+1);
+	       
+	        int ex = dfs2(w,p,capacity,currentsum,index+1);
+	       
+	        return Math.max(inc,ex);
 	    }
 	
 	
