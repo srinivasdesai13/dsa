830d68a96b6595030c46ffa56fc7d7a1b6fc3745
srinivasdesai13 <srinivasdesai.13@gmail.com>
2023-08-26 12:44:14 -0700

    Update FactorialCalculator.java

--- src/main/java/com/sd/dsa/dp/FactorialCalculator.java
+++ src/main/java/com/sd/dsa/dp/FactorialCalculator.java
@@ -1,6 +1,13 @@
 package com.sd.dsa.dp;
 
 public class FactorialCalculator {
+	
+	/*
+	 * Question: Implement a function to calculate the factorial of a non-negative
+	 * integer n. The factorial of n, denoted as n!, is the product of all positive
+	 * integers from 1 to n.
+	 * 
+	 */
 	public static int calculateFactorial(int n) {
 		if (n > 0) {
 			return n * calculateFactorial(n - 1);
