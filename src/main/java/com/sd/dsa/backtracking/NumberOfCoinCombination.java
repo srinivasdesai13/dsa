1b178b2ad282f3e6ff7484a608ddc73f977793bc
srinivasdesai13 <srinivasdesai.13@gmail.com>
2023-08-26 12:39:52 -0700

    Update NumberOfCoinCombination.java

--- src/main/java/com/sd/dsa/backtracking/NumberOfCoinCombination.java
+++ src/main/java/com/sd/dsa/backtracking/NumberOfCoinCombination.java
@@ -1,6 +1,23 @@
 package com.sd.dsa.backtracking;
 
 public class NumberOfCoinCombination {
+	
+	/*
+	 * Question: You are given an array of positive integers representing coin
+	 * denominations and a target amount. Write a function to compute the number of
+	 * distinct combinations that can make up the target amount using any number of
+	 * coins of each denomination. You can assume that you have an infinite supply
+	 * of coins for each denomination.
+	 * 
+	 * For example:
+	 * 
+	 * Coin denominations: [1, 2, 5] Target amount: 5 Output: 4 Explanation: There
+	 * are four ways to make up the amount 5: [1, 1, 1, 1, 1] [1, 1, 1, 2] [1, 2, 2]
+	 * [5] Write a function to solve this problem and return the number of distinct
+	 * combinations. If it's not possible to make up the target amount, return 0.
+	 * 
+	 */
+	
 
 	public static void main(String args[]) {
 	      int[] coins = {1, 3, 4};
